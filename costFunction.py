fixed bug with gradient, and loss function

costFunction.py
@@ -10,12 +10,13 @@ def linear_regression_loss_function(theta, X, y, lamda):
 
     g = np.dot(X, theta)
     h = sigmoid(g)
-    # loss = np.mean(np.multiply(-y , np.log(h)) - np.multiply((1 - y), np.log(1-h)))
-    loss = (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+    loss = np.mean(np.multiply(-y , np.log(h)) - np.multiply((1 - y), np.log(1-h)))
+    #loss = (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
     loss_regularization_term = ( lamda * np.sum(np.power(theta[1:], 2)) ) / (2 * m)
     loss = loss + loss_regularization_term
 
-    gradients = np.sum(np.dot(np.transpose(X), (h - y)), axis=0) / m
+    gradients = np.dot(np.transpose(X), (h - y))
+    gradients = gradients / m
     copy_theta = theta
     copy_theta[0] = 0
     loss_gradients_term = (lamda * copy_theta) / m

main.py
@@ -9,6 +9,8 @@ def normalize_data(data):
 def load_data(path, lines_to_take = 5000):
     df = np.array(pd.read_csv(path, nrows=lines_to_take))
     labels = df[:, 0]
+    labels = np.reshape(labels, (len(labels), 1))
+
     data = df[:, 1:]
     data = normalize_data(data)
     data = np.c_[np.ones(labels.shape[0]), data]
@@ -33,19 +35,37 @@ def main():
     labels, data = load_data(train_path)
     train_data, train_labels, cv_data, cv_labels = sperate_data_to_train_and_cv(data, labels)
 
-    num_of_iterations = 1000
+    num_of_iterations = 400
 
     # we initial theta to be zeros.
     # theta = np.random.rand(train_data.shape[1], 1)
-    theta = np.zeros((train_data.shape[1], 1))
-    alpha = 0.0009
+    theta = np.zeros((train_data.shape[1], 10))
+    alpha = 3.6
 
     for digit in range(10):
         current_labels = np.array(1 * (train_labels == digit))
+        temp_theta = np.zeros((train_data.shape[1], 1))
         for i in range(num_of_iterations):
-            loss, gradients = costFunction.linear_regression_loss_function(theta, train_data, current_labels, 0.03)
-            theta = theta - (alpha*gradients)
-            print 'Iter %d with Cost %f' %(i, loss)
+            loss, gradients = costFunction.linear_regression_loss_function(temp_theta, train_data, current_labels, 0.1)
+            temp_theta = temp_theta - (alpha*gradients)
+            #print 'Iter %d with Cost %f for digit %d' % (i, loss, digit)
+
+        print 'Cost %f for digit %d' % (loss, digit)
+        theta[:, digit] = temp_theta.reshape((len(temp_theta)))
+
+    # Calculate Errors on cross validation - useful for taking the best alpha and gama
+    #for digit in range(10):
+    #    current_labels = np.array(1 * (cv_labels == digit))
+    #    calculated_theta = theta[:, digit].reshape((len(temp_theta), 1))
+    #    loss, gradients = costFunction.linear_regression_loss_function(calculated_theta, cv_data, current_labels, 0.)
+    #   print 'Cost %f for digit %d on CROSS VALIDATION' %(loss, digit)
+
+    results = np.dot(cv_data, theta)
+    indexs = np.argmax(results, axis=1).reshape((len(cv_labels), 1))
+    total_errors = np.sum(indexs == cv_labels)
+    percentage = (total_errors / (1. * len(cv_labels)))
+    print 'Total error %d from %d (Success Rate of - %f percentage)'%(total_errors, len(cv_labels), percentage*100)
+
 
 
 